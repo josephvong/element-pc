ec7d88f989bd5c72ef3a6b5679b62a9937a12c75
josephvong <m15914375662@163.com>
2017-09-29 09:19:01 +0800

    20170929

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+const _import = require('./_import_' + process.env.NODE_ENV)
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      component: _import('login/index'),
+      hidden: true
     }
   ]
 })
