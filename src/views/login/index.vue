92d7f04bac1af8a09bcb9d582aaaf0f91af89a7e
josephvong <m15914375662@163.com>
2017-10-08 23:47:27 +0800

    20171008

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,11 +4,14 @@
       <h3 class="title">系统登录</h3>
 
       <el-form-item prop="username">
+       <em class='iconfont icon-user'></em>
        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="邮箱" />
+       <!--  -->
       </el-form-item>
 
       <el-form-item prop="password">
        <el-input name="password" type="text" v-model="loginForm.password" autoComplete="on" placeholder="密码" />
+       <em class='iconfont icon-password'></em>
       </el-form-item>
     </el-form>
   </div>
@@ -42,7 +45,7 @@ export default {
         username: 'admin',
         password: '111111'
       },
-      loginRule: {
+      loginRules: {
         // el-form 的 校验规则 validator 传入 一个函数 进行校验
         username: [{required: true, trigger: 'blur', validator: validateUsername}],
         password: [{required: true, trigger: 'blur', validator: validatePassword}]
@@ -63,9 +66,14 @@ export default {
 <style rel="stylesheet/stylus" lang="stylus" scoped>
 @import '~stylus/mixin.styl'
 $bg = #2d3a4b
+$dark_gray = #889aa4
+
 .login-container
   height 100vh
   background $bg
+  input:-webkit-autofill
+    -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
+    -webkit-text-fill-color: #fff !important;
   input
     background: transparent;
     border: 0px;
@@ -73,10 +81,18 @@ $bg = #2d3a4b
     border-radius: 0px;
     padding: 12px 5px 12px 15px;
     color: $light_gray;
-    height: 47px;
-  input:-webkit-autofill
-    -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
-    -webkit-text-fill-color: #fff !important;
+    height: 60px;  
+  .el-input
+    display inline-block
+    height 60px
+    width 85%
+  .iconfont
+    padding 6px 5px 6px 15px
+    color $dark_gray
+    vertical-align middle
+    width 30px
+    display inline-block
+    font-size 20px
   .title
     font-size: 26px;
     font-weight: 400;
@@ -91,19 +107,17 @@ $bg = #2d3a4b
     width: 400px;
     padding: 35px 35px 15px 35px;
     margin: 120px auto;
+    border 1px solid red
   .el-form-item
     border: 1px solid rgba(255, 255, 255, 0.1);
     background: rgba(0, 0, 0, 0.1);
     border-radius: 5px;
     color: #454545;
 
-  .el-input
-    display inline-block
-    height 47px
-    width 85%
-  .tips
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
+    
+    /*.tips
+      font-size: 14px;
+      color: #fff;
+      margin-bottom: 10px;*/
 
 </style>
